f2d1ba529599d6677cc34e77708afd9b236af960
mauLovera <mauricioloveraedu@gmail.com>
2022-06-09 13:35:49 -0400

    project module

---
+++ js/project-data.js
@@ -0,0 +1,13 @@
+const projectData = [
+  {
+    title: 'Tic-Tac-Toe',
+    deployment: 'https://tic-tac-toe-mauriciolovera.netlify.app/',
+    image: '/assets/TTT.png',
+    github: 'https://github.com/mauLovera/ttt-weekend',
+    language: 'JavaScript'
+  }
+]
+
+export {
+  projectData
+}
